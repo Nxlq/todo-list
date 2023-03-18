bb12bf1d623efca05644d43b78fd7c15cb59d7de
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-03-18 18:37:53 -0400

    add and style a quick form generated by chatgpt
    add functionality to open and close the modal for dev convenience in
    index.js... should refactor to the view lol

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import View from "./view";
 
 const newInput = document.querySelector("#new-todo-input");
 const newBtn = document.querySelector(".btn.add-new");
+const modal = document.querySelector(".modal");
 
 newBtn.addEventListener("click", () => {
-  console.log(newInput.value);
+  // show new todo form
+  modal.classList.remove("hidden");
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") modal.classList.add("hidden");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) modal.classList.add("hidden"); // Quick solution to close modal for dev purposes, probably a bad practice lol
 });
 
 const test1 = Model.createTodo(
